ea85bd5b76668446a0b383ffac851924cc1ea96d
gabriellukke <gabriellukke@outlook.com>
2021-07-09 13:07:43 -0300

    Start 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const App = () => {
-  return <div>Banca de Frutas</div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fruits: []
+    }
+  }
+
+  render () {
+    return (
+      <div>Banca de Frutas</div>
+    )
+  }
 }
 
 export default App;
